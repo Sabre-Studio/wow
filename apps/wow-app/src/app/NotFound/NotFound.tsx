3f5201659cbcd6762a19f733413cddf999385963
Victor Tantillo <v.tantillo3@gmail.com>
2023-04-11 23:19:43 -0600

    Starting react apps with new yarn, using packages instead of lib

--- apps/wow-app/src/app/NotFound/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-function NotFound() {
-  return <div></div>
-}
-
-export default NotFound
---
+++ packages/wow-db-schema/wow_db/schema.py
@@ -0,0 +1,59 @@
+import sqlalchemy as sql
+import sqlalchemy.dialects.postgresql as pg
+
+wow_db_metadata = sql.MetaData()
+
+# This eventually could have the region and stuff on it so it's just not the id
+connected_realms_table = sql.Table(
+    "connected_realms",
+    wow_db_metadata,
+    sql.Column("id", pg.INTEGER, primary_key=True),
+    sql.Column("href", pg.TEXT),
+    sql.Column("population", pg.TEXT),
+    sql.Column("mythic_leaderboards_href", pg.TEXT),
+    sql.Column("auctions_href", pg.TEXT),
+    sql.Column("updated_at", pg.DATE),
+)
+
+realms_table = sql.Table(
+    "realms",
+    wow_db_metadata,
+    sql.Column("id", pg.INTEGER, primary_key=True),
+    sql.Column("name", pg.TEXT),
+    sql.Column("href", pg.TEXT),
+    sql.Column("category", pg.TEXT),
+    sql.Column("locale", pg.TEXT),
+    sql.Column("timezone", pg.TEXT),
+    sql.Column("type", pg.TEXT),
+    sql.Column("slug", pg.TEXT),
+    sql.Column("is_tournament", pg.BOOLEAN),
+    sql.Column("connected_realm_id", pg.INTEGER),
+    sql.Column("region_id", pg.INTEGER),
+    sql.Column("updated_at", pg.DATE),
+)
+
+regions_table = sql.Table(
+    "regions",
+    wow_db_metadata,
+    sql.Column("id", pg.INTEGER, primary_key=True),
+    sql.Column("name", pg.TEXT),
+    sql.Column("href", pg.TEXT),
+    sql.Column("updated_at", pg.DATE),
+)
+
+mythic_keystone_season = sql.Table(
+    "mythic_keystone_seasons",
+    wow_db_metadata,
+    sql.Column("id", pg.INTEGER, primary_key=True),
+    sql.Column("start_timestamp", pg.TIMESTAMP),
+    sql.Column("end_timestamp", pg.TIMESTAMP),
+)
+
+mythic_keystone_period_table = sql.Table(
+    "mythic_keystone_periods",
+    wow_db_metadata,
+    sql.Column("id", pg.INTEGER, primary_key=True),
+    sql.Column("start_timestamp", pg.TIMESTAMP),
+    sql.Column("end_timestamp", pg.TIMESTAMP),
+    sql.Column("season_id", pg.INTEGER),
+)
